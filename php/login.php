4c7ddbbe8c89245e78529dd37d69822c603b8688
Ferenc Hammerl <ferenchammerl@outlook.com>
2014-11-25 17:14:30 +0100

    Login hotfixfix

--- php/login.php
+++ php/login.php
@@ -15,33 +15,5 @@ if (!empty($_POST)) {
 
 
 
-//
-//
-//    $username = $_POST['username'];
-//
-//    User::checkUsername($username);
-//    $salt = '';
-//    $dbpassword = '';
-//    User::fetchPassword($username, $salt, $dbpassword);
-//
-//
-//    $password = $_POST['password'];
-//    $password = hash('sha256', $password . $salt);
-//    for ($round = 0; $round < 65536; $round++) {
-//        $password = hash('sha256', $password . $salt);
-//    }
-//
-//
-//    if ($password == $dbpassword) {
-//        $_SESSION['username'] = $username;
-//        echo 'You are now logged in';
-//        $_SESSION['Title'] = 'Welcome, ' . $username;
-//        unset($_SESSION['loginfailure']);
-//        header("Location: mymatches.php");
-//    } else {
-//        $_SESSION['loginfailure'] = true;
-//        header("Location: ../index.php");
-//
-//    }
 }
 ?>
