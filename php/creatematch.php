d1bb78e158a4b2e093b388ca69231362ead6c8c6
Johannes Normann Jensen <jnj@iportalen.dk>
2014-11-25 16:25:26 +0100

    hotfix for matches

--- php/creatematch.php
+++ php/creatematch.php
@@ -19,7 +19,9 @@
         <div class="container">
             <div class="jumbotron2">
                 <div class="row">
-                    <div class="col-md-2 col-md-offset-5 text-center"><h2><span class="label label-success">Winners</span></h2></div>
+                    <div class="col-md-2 col-md-offset-5 text-center">
+                        <h2><span class="label label-success">Winners</span></h2>
+                    </div>
                 </div>
                 <div class="row">
                     <form class="form-horizontal" role="form">
@@ -43,7 +45,9 @@
                     </form>
                 </div>
                 <div class="row">
-                    <div class="col-md-2 col-md-offset-5 text-center"><h2><span class="label label-danger">Losers</span></h2></div>
+                    <div class="col-md-2 col-md-offset-5 text-center">
+                        <h2><span class="label label-danger">Losers</span></h2>
+                    </div>
                 </div>
                 <div class="row">
                     <form class="form-horizontal" role="form">
